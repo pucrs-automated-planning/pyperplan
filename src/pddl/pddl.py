48dad737c5f21c0d2efb2f6e6a64d6b9c1e3c375
Felipe Meneguzzi <felipe.meneguzzi@gmail.com>
2017-11-27 17:42:00 -0200

    Added PDDL conversion routines.

--- src/pddl/pddl.py
+++ src/pddl/pddl.py
@@ -144,3 +144,174 @@ class Problem:
                  [str(p) for p in self.goal]))
 
     __str__ = __repr__
+
+
+class PDDLWriter:
+
+    def __init__(self):
+        # 0 - domain name
+        # 1 - requirements
+        # 2 - types
+        # 3 - constants
+        # 4 - predicates
+        # 5 - actions
+        self.domain_template = "(define (domain {0})\n\n" \
+                               "  {1}\n\n" \
+                               "  {2}\n\n" \
+                               "  {3}\n\n" \
+                               "  {4}\n\n" \
+                               "  {5}" \
+                               ")"
+        # 0 - problem name
+        # 1 - domain name
+        # 2 - objects
+        # 3 - initial state
+        # 4 - goal state
+        self.problem_template = "(define (problem {0})\n " \
+                                "   (:domain {1})\n" \
+                                "   {2}\n" \
+                                "   {3}\n" \
+                                "   {4}\n" \
+                                ")"
+
+        self.requirements_template = "(:requirements {0})"
+        self.types_template = "(:types {0})"
+        self.constants_template = "(:constants {0})"
+        self.predicates_template = "(:predicates {0})"
+        self.and_template = "(and {0})"
+        self.not_template = "(not {0})"
+        # For actions
+        # 0 - name
+        # 1 - parameters
+        # 2 - precondition
+        # 3 - effect
+        # 4 - cost - not in use
+        self.action_template = "(:action {0}" \
+                               "   :parameters {1}" \
+                               "   :precondition {2}" \
+                               "   :effect {3}" \
+                               ")"
+
+    def write_requirements(self, domain):
+        assert isinstance(domain, Domain)
+        requirements = ":strips"
+        if domain.types: requirements += " :types"
+        return self.requirements_template.format(requirements)
+
+    def write_types(self, domain):
+        assert isinstance(domain, Domain)
+        if domain.types:
+            types = ""
+            for t in domain.types:
+                types += str(t)+" "
+            return self.types_template.format(types)
+        else:
+            return ""
+
+    def write_constants(self, domain):
+        assert isinstance(domain, Domain)
+        if domain.constants:
+            c_list = ""
+            for c in domain.constants:
+                c_list += repr(c)+"\n"
+            return self.constants_template.format(c_list)
+        else:
+            return ""
+
+    def write_predicates(self,domain):
+        assert isinstance(domain, Domain)
+        preds = ""
+        for p in domain.predicates:
+            preds += " "+self.pddl_predicate(domain.predicates[p])
+
+        return self.predicates_template.format(preds)
+
+    def write_actions(self,domain):
+        act = ""
+        for a in domain.actions:
+            act+=self.pddl_action(domain.actions[a])+"\n\n"
+        return act
+
+    def pddl_action(self,action):
+        assert isinstance(action, Action)
+        # print (str(action.name))
+        return self.action_template.format(action.name,
+                                           "("+self.pddl_signature(action.signature)+")",
+                                           self.pddl_precondition(action.precondition),
+                                           self.pddl_effect(action.effect))
+
+    def pddl_signature(self,signature, typed=False):
+        sig = ""
+        for o in signature:
+            sig += " "+(self.pddl_typed_object(o) if typed else self.pddl_untyped_object(o))
+        return sig
+
+
+
+    def pddl_precondition(self,precondition):
+        precond = ""
+        for literal in precondition:
+            precond += " "+self.pddl_literal(literal)
+        return self.and_template.format(precond)
+
+    def pddl_effect(self,effect):
+        assert isinstance(effect,Effect)
+        eff = ""
+        for literal in effect.addlist:
+            eff+=" "+self.pddl_literal(literal)
+        for literal in effect.dellist:
+            eff+=" "+self.pddl_literal(literal,False)
+        return self.and_template.format(eff)
+
+    def pddl_predicate(self, predicate):
+        assert isinstance(predicate, Predicate)
+        pars = self.pddl_signature(predicate.signature, True)
+        return "({0} {1})".format(predicate.name, pars)
+
+    def pddl_literal(self, literal, positive=True):
+        pars = self.pddl_signature(literal.signature)
+        lit = "({0} {1})".format(literal.name, pars)
+        if not positive:
+            lit = self.not_template.format(lit)
+        return lit
+
+    def pddl_untyped_object(self, o):
+        return o[0]
+
+    def pddl_typed_object(self, o):
+        return "{0} - {1}".format(o[0],o[1][0])
+
+    def write_domain(self, domain):
+        assert isinstance(domain, Domain)
+        return self.domain_template.format(
+            domain.name,
+            self.write_requirements(domain),
+            self.write_types(domain),
+            self.write_constants(domain),
+            self.write_predicates(domain),
+            self.write_actions(domain))
+
+    def write_problem(self, problem):
+        assert isinstance(problem, Problem)
+        objects = ""
+        for o in problem.objects:
+            if problem.domain.types:
+                objects += " {0} - {1}\n\t".format(o,str(problem.objects[o]))
+            else:
+                objects += " " + self.pddl_untyped_object(o)
+        objects = "(:objects {0})\n\n".format(objects)
+        init = ""
+        for literal in problem.initial_state:
+            init += " "+self.pddl_literal(literal)
+        init = "(:init {0})".format(init)
+
+        goal = ""
+        for literal in problem.goal:
+            goal += " " + self.pddl_literal(literal)
+        goal = "(:goal {0})".format(self.and_template.format(goal))
+
+        return self.problem_template.format(problem.name,
+                                            problem.domain.name,
+                                            objects,
+                                            init,
+                                            goal)
--- src/tests/test_pddl.py
+++ src/tests/test_pddl.py
@@ -1,22 +1,106 @@
-from pddl.pddl import Type, Predicate, Domain, Problem
+from pddl.pddl import Type, Predicate, Domain, Problem, PDDLWriter
+from pddl.parser import Parser
+from pddl.lisp_parser import parse_lisp_iterator
 
 
 def test_type():
     test = Type("test", "parent")
-    assert(str(test) == repr(test))
+    assert (str(test) == repr(test))
 
 
 def test_predicate():
     test = Predicate("test", list(("a", list())))
-    assert(str(test) == repr(test))
+    assert (str(test) == repr(test))
 
 
 def test_domain():
     test = Domain('domain', dict(), list(), list(), dict())
-    assert(str(test) == repr(test))
+    assert (str(test) == repr(test))
 
 
 def test_problem():
     domain = Domain('domain', dict(), list(), list(), dict())
     test = Problem('problem', domain, dict(), list(), list())
-    assert(str(test) == repr(test))
+    assert (str(test) == repr(test))
+
+
+def test_writer_simple():
+    domain = Domain('domain1', {}, [], [], {})
+    problem = Problem('problem1', domain, dict(), list(), list())
+    writer = PDDLWriter()
+    # print(writer.write_domain(domain))
+    # print(writer.write_problem(problem))
+
+
+def test_writer_complex():
+    test = ["""
+        (define (domain BLOCKS)
+      (:requirements :strips :typing)
+      (:types block)
+      (:predicates (on ?x - block ?y - block)
+                   (ontable ?x - block)
+                   (clear ?x - block)
+                   (handempty)
+                   (holding ?x - block)
+                   )
+
+      (:action pick-up
+                 :parameters (?x - block)
+                 :precondition (and (clear ?x) (ontable ?x) (handempty))
+                 :effect
+                 (and (not (ontable ?x))
+                       (not (clear ?x))
+                       (not (handempty))
+                       (holding ?x)))
+
+      (:action put-down
+                 :parameters (?x - block)
+                 :precondition (holding ?x)
+                 :effect
+                 (and (not (holding ?x))
+                       (clear ?x)
+                       (handempty)
+                       (ontable ?x)))
+      (:action stack
+                 :parameters (?x - block ?y - block)
+                 :precondition (and (holding ?x) (clear ?y))
+                 :effect
+                 (and (not (holding ?x))
+                       (not (clear ?y))
+                       (clear ?x)
+                       (handempty)
+                       (on ?x ?y)))
+      (:action unstack
+                 :parameters (?x - block ?y - block)
+                 :precondition (and (on ?x ?y) (clear ?x) (handempty))
+                 :effect
+                 (and (holding ?x)
+                       (clear ?y)
+                       (not (clear ?x))
+                       (not (handempty))
+                       (not (on ?x ?y)))))
+        """,
+            """
+                (define (problem BLOCKS-4-0)
+                (:domain BLOCKS)
+                (:objects D B A C - block)
+                (:INIT (CLEAR C) (CLEAR A) (CLEAR B) (CLEAR D) (ONTABLE C) (ONTABLE A)
+                (ONTABLE B) (ONTABLE D) (HANDEMPTY))
+                (:goal (AND (ON D C) (ON C B) (ON B A)))
+                )
+                """
+            ]
+    parser = Parser(None)
+    parser.domInput = test[0]
+    parser.probInput = test[1]
+    domain = parser.parse_domain(False)
+    problem = parser.parse_problem(domain,False)
+    writer = PDDLWriter()
+    domain_string = writer.write_domain(domain)
+    # print(domain_string)
+    parser.domInput = domain_string
+    domain = parser.parse_domain(False) # Checking if our output is valid pddl
+    assert domain is not None
+    # print(writer.write_domain(domain))
+    # assert domain_string == writer.write_domain(domain)
+    print(writer.write_problem(problem))
